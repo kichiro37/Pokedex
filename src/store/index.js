11776e87c5605a4a81d557e8be7c59f4a5cc4499
M Khoirul Rizal <khoirul@mggosftware.com>
2021-06-01 23:58:21 +0700

    feat: infinite scroll

--- src/store/index.js
+++ src/store/index.js
@@ -10,25 +10,31 @@ export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
    	state: {
    		vueInstalled: false, 
-   		pokemons: []
+   		pokemons: [],
+      pokeNextUrl: '/pokemon'
    	},
    	mutations: {
    		updateVueInstalled(state, arg1) {
    			state.vueInstalled = arg1
    		},
-   		updatePokemons(state, pokemons) {
-   			state.pokemons = pokemons
-   			console.log('updatePokemons 1', state.pokemons)
-   		}
+   		fetchPokemons(state, pokemons) {
+   			state.pokemons = [...state.pokemons, ...pokemons]
+   			console.log('fetchPokemons 1', state.pokemons)
+   		},
+      updatePokeNextUrl(state, pokeNextUrl) {
+        state.pokeNextUrl = pokeNextUrl
+      }
    	},
    	actions: {
    		toggleVue({state, commit, dispatch}, arg1 ) {
    			commit('updateVueInstalled', !state.vueInstalled)
    		},
-   		GetPokemons({commit}) {
-   			axios.get('/pokemon')
+   		GetPokemons({state, commit}) {
+   			axios.get(state.pokeNextUrl)
    			.then(resp => {
-   				commit('updatePokemons', resp.data.results)
+          console.log('GetPokemons', resp)
+          commit('updatePokeNextUrl', resp.data.next)
+   				commit('fetchPokemons', resp.data.results)
    			})
    			.catch(err => {
    			})
